ce54073263628975717f88c071d82b4ba659b708
Jorge Hurtado <jorge@radiotecpro.com>
2026-05-28 15:40:45 -0700

    fix(omnichat): change whatsapp webVersionCache from remote to local to prevent hang

--- backend/src/whatsapp/whatsapp.service.ts
+++ backend/src/whatsapp/whatsapp.service.ts
@@ -118,8 +118,7 @@ export class WhatsappService implements OnModuleInit {
     const client = new Client({
       authStrategy: new LocalAuth({ clientId: companyId, dataPath: './.wwebjs_auth' }),
       webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        type: 'local',
       },
       puppeteer: {
         headless: true,
