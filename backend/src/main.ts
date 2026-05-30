d002e6fc5de906bb9461622ff373979276684a81
Jorge Hurtado <jorge@radiotecpro.com>
2026-05-30 00:00:29 -0700

    fix(backend): allow cross-origin resource policy in helmet to prevent browsers from blocking images loaded via API subdomain on the frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,9 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.use(helmet());
+  app.use(helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+  }));
   const cookieParser = require('cookie-parser');
   app.use(cookieParser());
   
